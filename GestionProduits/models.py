3c0ca200b0f064686e332de69a52d390c83b08d4
ProFMJ3 <follykoejules@gmail.com>
2025-02-01 12:32:29 +0000

    Sauvegarde des modifications locales

--- GestionProduits/models.py
+++ GestionProduits/models.py
@@ -60,9 +60,72 @@ NB:Si on prends une catégorie, on va avoir plusieurs articles et un article app
 - quantite
 - prixTotal
 - client(FK)
-- article(FK)
+- Liste Achats(FK)
 
 
 
 
 """
+
+#Model catégorie
+
+class Categorie(models.Models):
+    titre = models.CharField(max_length=255)
+    description = models.TextField()
+    dateAjout = models.DateTimeField(auto_now_add=True)
+
+
+    class Meta:
+        verbose_name = "Categorie"
+        verbose_name_plural = "Categories"
+        ordering("-dateAjout")
+    def __init__(self):
+        return self.titre
+
+class Article(models.Models):
+    nom = models.CharField(max_length=255)
+    quantite = models.IntegerField()
+    image = models.ImageField(upload_to="imagesArticles/")
+    prixUnitaire = models.DecimalField(max_digits=10, decimal_places=2)
+    dateAjout = models.DateTimeField(auto_now_add=True)
+    dateModification = models.DateTimeField(auto_now=True)
+    categorie = models.ForeignKey(Categorie, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "Article"
+        verbose_name_plural = "Articles"
+        #ordering("-dateAjout")
+        ordering=['-dateAjout']
+    def __str__(self):
+        return self.nom
+
+class Client(models.Models):
+    prenom = models.CharField(max_length=255)
+    nom = models.CharField(max_length=255)
+    adresse = models.TextField()
+    telephone = models.CharField(max_length=15)
+    dateInscription = models.DateTimeField(auto_now_add=True)
+
+
+class Panier(models.Model):
+    dateAjout = models.DateTimeField(auto_now_add=True)
+    dateModification = models.DateTimeField(auto_now=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    Achats = models.ManyToManyField(Client, trough="achats")
+
+
+class achats(models.Model):
+    dateCommande = models.DateTimeField(auto_now_add=True)
+    quantite = models.IntegerField()
+    prixTotal = models.DecimalField(max_digits=10, decimal_places=2)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    panier = models.ForeignKey(Panier, on_delete=models.CASCADE)
+    class Meta:
+        verbose_name = "Commande"
+        verbose_name_plural = "Commandes"
+        ordering["-dateCommande"]
+
+
+    def __str__(self):
+        return self.article.nom
